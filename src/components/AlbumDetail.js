ce04e3e812c9fa3775ce7c9c8856df508ef0329e
lauren.madigan <lauren.madigan@workday.com>
2018-02-26 23:04:37 -0800

    link to website/ button style

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import CardSection from './CardSection';
 import Card from './Card';
 import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const { thumbnailStyle, headerContentStyles,
     thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
   return (
@@ -27,7 +27,9 @@ const AlbumDetail = ({ album }) => {
         source={{ uri: image }} />
     </CardSection>
     <CardSection>
-      <Button onPress={() => console.log(title)} />
+      <Button onPress={() => Linking.openURL(url)}>
+          Buy Now!
+        </Button>
     </CardSection>
   </Card>
 
